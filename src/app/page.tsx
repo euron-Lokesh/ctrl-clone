475826da0c45291e3b3a71a65af5475951806775
euron-Lokesh <lokesh@euron.one>
2025-10-22 01:03:49 +0530

    feat: add StartFromTop component to Home page for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,14 @@ import FloatingDownloadBtn from "@/components/sections/FloatingDownloadBtn";
 import HeroSection from "@/components/sections/HeroSection";
 import Overview from "@/components/sections/Overview";
 import SecurityHighlights from "@/components/sections/SecurityHighlights";
+import StartFromTop from "@/components/StartFromTop";
 import { AnimationProvider } from "@/context/AnimationContext";
 
 export default function Home() {
   return (
     <AnimationProvider>
+      <StartFromTop />
+
       <Header />
       <HeroSection BadgeIcon={<CtrlBadgeIcon size={120} />} text="Take Ctrl" />
       <FloatingDownloadBtn />
